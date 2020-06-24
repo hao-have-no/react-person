376948b87cf8e181e088283153c64b11a19c060e
fenghao <fenghao@infisa.cn>
2020-06-24 19:50:35 +0800

    npx 生成　react project,初试

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./static/iconfont/iconfont.css"
+// import * as serviceWorker from './serviceWorker';
 
+//Jsx需要babel loader 转义
+
+//把组件生成的真实dom挂载到root上
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
---
+++ src/pages/indexPage/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class IndexPage extends Component {
+    render() {
+        return (
+            <div>
+                <h3>IndexPage</h3>
+            </div>
+        );
+    }
+}
+
+export default IndexPage;
