882f6f37114073b266b3493fef49e9c352086585
hao-have-no <932537602@qq.com>
2020-06-30 17:24:45 +0800

    Update index.js
    增加对state的说明,state可以为对象

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,15 @@
-import {createStore} from "redux";
+    import {createStore} from "redux";
 
 //payload 根据数据进行变化
 //定义state初始化和修改规则　reducer是一个纯函数
 //action提交，对action进行识别处理，返回新的state
 //{type,payload}就是一个action对象
+// state可以为对象
 export const counterReducer =(state = 0, {type,payload}) =>{
     switch (type) {
-        case 'ADD':
+        case 'ADD':  
+            //return {...state,...newState}
+            // return {{num:1},{num:2}}
             return payload + state;
         case 'MINUS':
             return state - payload;
